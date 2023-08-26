81c17193a0d5dc971ddb2acc43f3b021842f0afc
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2023-08-26 13:03:54 -0400

    Add scripts directory with some useful scripts

---
+++ scripts/compile-tofino1.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+if [ "${SDE}" = "" -o "$SDE_INSTALL" = "" ]
+then
+    1>&2 echo "Source your setup script that sets env variables SDE and SDE_INSTALL"
+    exit 1
+fi
+echo "Found directory SDE=${SDE} ..."
+echo "Found directory SDE_INSTALL=${SDE_INSTALL} ..."
+P4GUIDE="$HOME/p4-guide"
+if [ ! -d "${P4GUIDE}" ]
+then
+    1>&2 echo "No directory ${P4GUIDE} found.  Create a copy of the repo https://github.com/jafingerhut/p4-guide there."
+    exit 1
+fi
+echo "Found directory P4GUIDE=${P4GUIDE} ..."
+STDLIB_INC_DIR="${P4GUIDE}/stdlib"
+
+if [ $# -ne 1 ]
+then
+    1>&2 echo "usage: $0 prog.p4"
+    exit 1
+fi
+BASENAME=`basename $1 .p4`
+
+bf-p4c \
+    -DTOFINO1 \
+    -b tofino \
+    -a tna \
+    --verbose 3 \
+    -I${STDLIB_INC_DIR} \
+    --program-name ${BASENAME} \
+    ${BASENAME}.p4
+
+exit_status=$?
+echo "bf-p4c exit status: ${exit_status}"
